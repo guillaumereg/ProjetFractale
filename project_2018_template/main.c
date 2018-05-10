1f9139f84c9341c0d089e1888ccb48380407857c
reginsterg <you@example.com>
2018-05-10 14:59:25 +0200

    voila

--- project_2018_template/main.c
+++ project_2018_template/main.c
@@ -24,7 +24,6 @@ int SIZE_FRACTALE = sizeof(struct fractal);
 char * filename; /* fichier d'entrée qu'on lit actuellement */
 char * fichierSortie; /* fichier de sortie */
 int plusieursFichiers = 0; /* pour savoir il s'il faut un fichier pour chaque fractale */
-char * tabFractale[100]; /* stocker le nom de toutes les fractales pour vérifier qu'aucune n'est identique */
 
 double plusGrandeMoyenne = 0; /* stocke la plus grande moyenne de fractale actuelle */
 struct fractal * fracMax; /* pointeur vers la fractale avec la plus grande moyenne */
@@ -69,7 +68,7 @@ int main(int argc, char *argv[]){
   /*vérifications nombres arguments si option -d activée en premier*/
   if (strcmp(argv[1], "-d")==0){
     i++;
-    if(argc<4){
+    if(argc<3){
       printf("Trop petit nombre d'arguments\n");
       exit(EXIT_FAILURE);
     }
@@ -77,7 +76,7 @@ int main(int argc, char *argv[]){
     /*vérifications nombres arguments si option -maxthreads activée en plus de l'option -d*/
     if (strcmp(argv[2], "--maxthreads")==0){
       i=i+2;
-      if(argc<6){
+      if(argc<5){
         printf("Trop petit nombre d'arguments\n");
         exit(EXIT_FAILURE);
       }
@@ -93,9 +92,9 @@ int main(int argc, char *argv[]){
       }
       maxThreads = atoi(argv[2]);
       /*vérifications nombres arguments si option -d activée en plus de l'option -maxthreads*/
-      if (strcmp(argv[2], "-d")==0){
+      if (strcmp(argv[2], "--maxthreads")==0){
         i++;
-        if(argc<6){
+        if(argc<5){
           printf("Trop petit nombre d'arguments\n");
           exit(EXIT_FAILURE);
         }
@@ -103,8 +102,12 @@ int main(int argc, char *argv[]){
       }
   }
 
-  fichierSortie = argv[argc-1];
-
+  /*Y a t'il un fichier de sortie?*/
+  int restant = 0;
+  if (plusieursFichiers == 0){
+    fichierSortie = argv[argc-1];
+    restant = 1;
+  }
 
   /*Lecture des fractales*/
   while (i<argc-restant){
@@ -127,13 +130,20 @@ int main(int argc, char *argv[]){
 
     pthread_create (&monThreadLecteur, NULL, threadLecteur, (void *)NULL);
     pthread_create (&monThreadCalculateur, NULL, threadCalculateur, (void *)NULL);
-    pthread_create (&monThreadEcrivain, NULL, threadEcrivain, (void *)NULL);
+    if (plusieursFichiers == 1){
+      pthread_create (&monThreadEcrivain, NULL, threadEcrivain, (void *)NULL);
+    }
 
     /*initialisation des buffer*/
     sbuf_init(buffer_lecteur_calculateur,4);
     sbuf_init(buffer_calculateur_ecrivain,4);
 
   }
+
+  if(plusieursFichiers == 0){
+    write_bitmap_sdl(fracMax, fichierSortie);
+  }
+
   return EXIT_SUCCESS;
 }
 
@@ -178,17 +188,6 @@ void *threadLecteur(void* arg){
         double a = atoi(tableChaine[3]);
         double b = atoi(tableChaine[4]);
         struct fractal * fracActu = fractal_new(name,width,height,a,b);
-
-        int n=0;
-        while(tabFractale[n]!=""){
-          if (tableChaine[0]== tabFractale[n]){
-            pthread_exit(NULL);
-          }
-          n++;
-        }
-        if(tabFractale[n+1]==""){
-          char * name = tabFractale[n];
-        }
 
         sbuf_insert(buffer_lecteur_calculateur, fracActu);
 
@@ -245,32 +244,22 @@ void * threadEcrivain(void* arg){
 
   while (bool == 0){
 
-
     struct fractal * fracActu = sbuf_remove(buffer_calculateur_ecrivain);
     if (fracActu == NULL){
       bool = 1;
       pthread_exit(NULL); /* Fin du thread */
     }
+    
     char * fichier = NULL;
-
-    if (plusieursFichiers == 0){
-
-      fichier = fichierSortie;
-
-    }else {
-
-      fichier = fracActu->name;
-
-    }
+    fichier = fracActu->name;
 
     if (fichier == NULL){
       printf("Erreur dans le fichier de sortie \n");
       exit(EXIT_FAILURE);
     }
-
     write_bitmap_sdl(fracActu, fichier);
-  }
 
+  }
   pthread_exit(NULL); /* Fin du thread */
 }
 
